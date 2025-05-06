682d2727b27ad7ae7dec3256b3583d438edda520
g1111wths <panagiotisfloros2024@gmail.com>
2025-05-06 15:02:01 +0200

    Doors Work again

--- Purrfect Escape/Assets/Scripts/Teleporter.cs
+++ Purrfect Escape/Assets/Scripts/Teleporter.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teleporter : MonoBehaviour
+public class Teleporter : MonoBehaviour
 {
     [SerializeField] private Transform destination;
 
     public Transform GetDestination()
- {
+    {
         return destination;
     }
+
 }
