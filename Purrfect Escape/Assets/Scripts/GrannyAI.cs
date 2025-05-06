3133b42fc5e61fc2b027c8bd66c6138b9dc6db38
CrackTail <XxCrackTailxX@gmail.com>
2025-05-06 22:10:15 +0200

    Granny Now Fights Back
    Granny Ai touching Cat grants Game Over Screen.

--- Purrfect Escape/Assets/Scripts/GrannyAI.cs
+++ Purrfect Escape/Assets/Scripts/GrannyAI.cs
@@ -1,47 +1,38 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
 
-public class GrannyAI:MonoBehaviour
+public class GrannyAI : MonoBehaviour
 {
-    public GameObject pointA;
-    public GameObject pointB;
-    private Animator anim;
-    private Rigidbody2D rb;
+    public GameObject pointA, pointB, gameOverPanel;
+    public float speed, newPointBX;
+    private bool doorMoved = false;
     private Transform currentPoint;
-    public float speed;
+    private Rigidbody2D rb;
+    private Animator anim;
 
-    private void Start()
+    void Start()
     {
-        rb=GetComponent<Rigidbody2D>();
-        anim=GetComponent<Animator>();
-        currentPoint=pointB.transform;
-        //anim.SetBool("isRunning", true);
-
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        // anim.SetBool("isRunning", true);
     }
-    private void Update()
+
+    void Update()
     {
-        Vector2 point=currentPoint.position-transform.position;
-        if (currentPoint== pointB.transform){
-            rb.linearVelocity = new Vector2(speed, 0);
-        }
-        else
+        rb.linearVelocity = new Vector2(currentPoint == pointB.transform ? speed : -speed, 0);
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
+            currentPoint = currentPoint == pointB.transform ? pointA.transform : pointB.transform;
+
+        if (!doorMoved && GameObject.FindWithTag("LockedDoor") == null)
         {
-            rb.linearVelocity = new Vector2(-speed, 0);
-        }
-        if (Vector2.Distance(transform.position, currentPoint.position)<0.5f && currentPoint==pointB.transform)
-        {
-            currentPoint = pointA.transform;
-        }
-        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
-        {
-            currentPoint = pointB.transform;
+            doorMoved = true;
+            pointB.transform.position = new Vector3(newPointBX, pointB.transform.position.y, pointB.transform.position.z);
         }
     }
-    private void OnDrawGizmos()
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
-        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
-        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
-}
+        if (other.CompareTag("Player"))
+            gameOverPanel.SetActive(true);
+    }
 }
--- Purrfect Escape/Assets/Scripts/PickUp.cs
+++ Purrfect Escape/Assets/Scripts/PickUp.cs
@@ -4,19 +4,20 @@ public class PickUp : MonoBehaviour
 {
     private FishCounter fishCounter;
     private GameObject fishToDestroy;
-
     void Start() => fishCounter = FindAnyObjectByType<FishCounter>();
-
     void Update()
     {
         if (fishToDestroy && Input.GetKeyDown(KeyCode.E))
         {
             fishCounter.AddFish();
             Destroy(fishToDestroy);
+            GameObject door = GameObject.FindWithTag("LockedDoor");
+            if (door != null)
+            {
+                Destroy(door);
+            }
         }
     }
-
     void OnTriggerEnter2D(Collider2D other) => fishToDestroy = other.CompareTag("Fish") ? other.gameObject : null;
-
     void OnTriggerExit2D(Collider2D other) => fishToDestroy = other.CompareTag("Fish") ? null : fishToDestroy;
 }
