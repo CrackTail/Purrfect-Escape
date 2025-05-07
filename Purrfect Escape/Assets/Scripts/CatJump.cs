ba7c209e332354fe91c18d1ee356086893e0e344
Nagi408 <Nathan.Wathelet77@gmail.com>
2025-05-07 10:31:20 +0200

    CatJump got a 1 sec cooldown now

--- Purrfect Escape/Assets/Scripts/CatJump.cs
+++ Purrfect Escape/Assets/Scripts/CatJump.cs
@@ -4,10 +4,17 @@ public class CatJump : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D Rigidbody2D;
     [SerializeField] private float JumpForce = 6;
-    
+    private float JumpCooldown = 1.0f;
+    private float LastJumpTime;
+
+    void Start()
+    {
+        LastJumpTime = -JumpCooldown;
+    }
+
     void Update()
     {
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && Time.time >= LastJumpTime + JumpCooldown)
         {
             Jump();
         }
@@ -15,5 +22,6 @@ public class CatJump : MonoBehaviour
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        LastJumpTime = Time.time;
     }
 }
