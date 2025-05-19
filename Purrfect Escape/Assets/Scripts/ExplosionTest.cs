ef00fbd4fbc1ec19961fcf926a44121393e15687
CrackTail <XxCrackTailxX@gmail.com>
2025-05-19 09:42:44 +0200

    Explosion Test script finnaly works after insane amount of time

--- Purrfect Escape/Assets/Scripts/ExplosionTest.cs
+++ Purrfect Escape/Assets/Scripts/ExplosionTest.cs
@@ -28,43 +28,60 @@ public class ExplodeOnImpact : MonoBehaviour
     {
         exploded = true;
         CreatePieces();
-        gameObject.SetActive(false);
+        gameObject.SetActive(false); // Hide the original lamp
         Invoke("DisablePhysics", explosionDuration);
     }
 
     void CreatePieces()
     {
-        float pieceWidth = originalSpriteRenderer.bounds.size.x / gridWidth;
-        float pieceHeight = originalSpriteRenderer.bounds.size.y / gridHeight;
+        Texture2D originalTexture = originalSpriteRenderer.sprite.texture;
+        Rect spriteRect = originalSpriteRenderer.sprite.textureRect;
+        float pixelsPerUnit = originalSpriteRenderer.sprite.pixelsPerUnit;
+
+        int piecePixelWidth = Mathf.RoundToInt(spriteRect.width / gridWidth);
+        int piecePixelHeight = Mathf.RoundToInt(spriteRect.height / gridHeight);
 
         for (int x = 0; x < gridWidth; x++)
         {
             for (int y = 0; y < gridHeight; y++)
             {
-                Vector3 position = new Vector3(transform.position.x + (x * pieceWidth), transform.position.y + (y * pieceHeight), transform.position.z);
-                CreatePiece(position, pieceWidth, pieceHeight);
+                int pixelX = Mathf.RoundToInt(spriteRect.x + x * piecePixelWidth);
+                int pixelY = Mathf.RoundToInt(spriteRect.y + y * piecePixelHeight);
+
+                Color[] pixelBlock = originalTexture.GetPixels(pixelX, pixelY, piecePixelWidth, piecePixelHeight);
+
+                Texture2D pieceTexture = new Texture2D(piecePixelWidth, piecePixelHeight);
+                pieceTexture.SetPixels(pixelBlock);
+                pieceTexture.Apply();
+
+                Sprite pieceSprite = Sprite.Create(pieceTexture, new Rect(0, 0, piecePixelWidth, piecePixelHeight), new Vector2(0.5f, 0.5f), pixelsPerUnit);
+
+                float worldX = ((float)piecePixelWidth / pixelsPerUnit) * (x - gridWidth / 2f + 0.5f);
+                float worldY = ((float)piecePixelHeight / pixelsPerUnit) * (y - gridHeight / 2f + 0.5f);
+                Vector3 piecePosition = transform.position + new Vector3(worldX, worldY, 0f);
+
+                CreatePiece(piecePosition, pieceSprite);
             }
         }
     }
 
-    void CreatePiece(Vector3 position, float width, float height)
+    void CreatePiece(Vector3 position, Sprite pieceSprite)
     {
         GameObject piece = new GameObject("Piece");
-        piece.transform.SetParent(transform);
-        SpriteRenderer pieceRenderer = piece.AddComponent<SpriteRenderer>();
-        pieceRenderer.sprite = originalSpriteRenderer.sprite;
         piece.transform.position = position;
-        piece.transform.localScale = new Vector3(width / originalSpriteRenderer.sprite.bounds.size.x, height / originalSpriteRenderer.sprite.bounds.size.y, 1);
+
+        SpriteRenderer pieceRenderer = piece.AddComponent<SpriteRenderer>();
+        pieceRenderer.sprite = pieceSprite;
+        pieceRenderer.sortingOrder = originalSpriteRenderer.sortingOrder;
 
         Rigidbody2D rb = piece.AddComponent<Rigidbody2D>();
         rb.gravityScale = 1;
         rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
 
         BoxCollider2D collider = piece.AddComponent<BoxCollider2D>();
-        collider.size = new Vector2(width, height);
+        collider.size = pieceSprite.bounds.size;
 
         int explosionLayer = LayerMask.NameToLayer("ExplosionPieces");
-
         if (explosionLayer == -1)
         {
             Debug.LogError("Layer 'ExplosionPieces' not found. Please create this layer in the Tags & Layers section of the Project Settings.");
@@ -79,7 +96,7 @@ public class ExplodeOnImpact : MonoBehaviour
         float randomY = Random.Range(0.5f, bounceStrength);
         rb.linearVelocity = new Vector2(randomX, randomY);
 
-        //Destroy(piece, 5f);
+        Destroy(piece, 5f); // Optional: Clean up after 5 seconds
     }
 
     void DisablePhysics()
