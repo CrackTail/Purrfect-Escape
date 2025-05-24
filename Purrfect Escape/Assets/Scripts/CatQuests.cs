4431deffc895cb50d71c32c2905c2181f47c0fb6
CrackTail <XxCrackTailxX@gmail.com>
2025-05-24 16:48:37 +0200

    Cat Quest now is a univeral code applicable to all cats at once.

---
+++ Purrfect Escape/Assets/Scripts/CatQuests.cs
@@ -0,0 +1,117 @@
+using UnityEngine;
+using TMPro;
+
+public class CatQuest : MonoBehaviour
+{
+    public GameObject dialogueBubble;
+    public TextMeshProUGUI dialogueText;
+    public GameObject NPCGives;
+    public GameObject NPCTakes;
+
+    [SerializeField] private string dialogueBefore = "Default request message";
+    [SerializeField] private string dialogueAfter = "Default reward message";
+    [SerializeField] private float fontSizeBefore = 36f;
+    [SerializeField] private float fontSizeAfter = 36f;
+
+    public enum RequiredItem { Fish, Pendant, Anger }
+    [SerializeField] private RequiredItem requiredItem;
+
+    public enum RewardItem { GoldKey, PinkKey, RustyKey }
+    [SerializeField] private RewardItem rewardItem;
+
+    private bool playerInRange = false;
+    private bool hasReceivedItem = false;
+
+    private PlayerInventory inventory;
+
+    void Start()
+    {
+        inventory = FindAnyObjectByType<PlayerInventory>();
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if (!hasReceivedItem && HasRequiredItem())
+            {
+                ReceiveItem();
+            }
+
+            ShowDialogue();
+        }
+    }
+
+    void ShowDialogue()
+    {
+        dialogueBubble.SetActive(true);
+
+        if (hasReceivedItem)
+        {
+            dialogueText.text = dialogueAfter;
+            dialogueText.fontSize = fontSizeAfter;
+        }
+        else
+        {
+            dialogueText.text = dialogueBefore;
+            dialogueText.fontSize = fontSizeBefore;
+        }
+    }
+
+    bool HasRequiredItem()
+    {
+        switch (requiredItem)
+        {
+            case RequiredItem.Fish:
+                return inventory.hasFish;
+            case RequiredItem.Pendant:
+                return inventory.hasPendant;
+            case RequiredItem.Anger:
+                return inventory.hasAnger;
+            default:
+                return false;
+        }
+    }
+
+    void ReceiveItem()
+    {
+        hasReceivedItem = true;
+
+        switch (requiredItem)
+        {
+            case RequiredItem.Fish:
+                inventory.hasFish = false;
+                Debug.Log("Item taken: Fish");
+                break;
+            case RequiredItem.Pendant:
+                inventory.hasPendant = false;
+                Debug.Log("Item taken: Pendant");
+                break;
+            case RequiredItem.Anger:
+                inventory.hasAnger = false;
+                Debug.Log("Item taken: Anger");
+                break;
+        }
+
+        if (NPCTakes != null)
+            Destroy(NPCTakes);
+
+        if (NPCGives != null)
+            NPCGives.SetActive(true); // NPCGives should be the item to spawn in the scene
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            dialogueBubble.SetActive(false);
+        }
+    }
+}
--- Purrfect Escape/Assets/Scripts/InventoryTracker.cs
+++ Purrfect Escape/Assets/Scripts/InventoryTracker.cs
@@ -2,6 +2,76 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
-    public bool hasFish = false;
-    public bool hasKey = false;
+    private bool _hasFish = false;
+    private bool _hasPendant = false;
+    private bool _hasAnger = false;
+    private bool _hasKey = false;
+    private bool _hasPinkKey = false;
+    private bool _hasRustyKey = false;
+
+    public bool hasFish
+    {
+        get => _hasFish;
+        set
+        {
+            if (!_hasFish && value)
+                Debug.Log("Collected Fish!");
+            _hasFish = value;
+        }
+    }
+
+    public bool hasPendant
+    {
+        get => _hasPendant;
+        set
+        {
+            if (!_hasPendant && value)
+                Debug.Log("Collected Pendant!");
+            _hasPendant = value;
+        }
+    }
+
+    public bool hasAnger
+    {
+        get => _hasAnger;
+        set
+        {
+            if (!_hasAnger && value)
+                Debug.Log("Collected Anger!");
+            _hasAnger = value;
+        }
+    }
+
+    public bool hasKey
+    {
+        get => _hasKey;
+        set
+        {
+            if (!_hasKey && value)
+                Debug.Log("Collected Gold Key!");
+            _hasKey = value;
+        }
+    }
+
+    public bool hasPinkKey
+    {
+        get => _hasPinkKey;
+        set
+        {
+            if (!_hasPinkKey && value)
+                Debug.Log("Collected Pink Key!");
+            _hasPinkKey = value;
+        }
+    }
+
+    public bool hasRustyKey
+    {
+        get => _hasRustyKey;
+        set
+        {
+            if (!_hasRustyKey && value)
+                Debug.Log("Collected Rusty Key!");
+            _hasRustyKey = value;
+        }
+    }
 }
