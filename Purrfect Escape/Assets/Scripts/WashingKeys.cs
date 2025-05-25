76410ff2c4054fc73dca2e7cf2974768c31dbdb6
Nagi408 <Nathan.Wathelet77@gmail.com>
2025-05-25 16:28:34 +0200

    Cleaning icons added an almost fixed with washing script

--- Purrfect Escape/Assets/Scripts/WashingKeys.cs
+++ Purrfect Escape/Assets/Scripts/WashingKeys.cs
@@ -5,10 +5,7 @@ public class WashingKeys : MonoBehaviour
     [SerializeField] private KeyCode interactionKey = KeyCode.E;
     [SerializeField] private float interactionRange = 4f;
 
-    [SerializeField] private GameObject interactionIconPrefab;
-
     private GameObject player;
-    private GameObject interactionIconInstance;
 
     private void Start()
     {
@@ -20,34 +17,18 @@ public class WashingKeys : MonoBehaviour
         if (player == null) return;
 
         float distance = Vector2.Distance(transform.position, player.transform.position);
-        bool isInRange = distance <= interactionRange;
-        if (isInRange)
+        if (distance <= interactionRange)
         {
-            if (interactionIconInstance == null && interactionIconPrefab != null)
+            if (Input.GetKeyDown(interactionKey))
             {
-                interactionIconInstance = Instantiate(interactionIconPrefab, player.transform);
-                interactionIconInstance.transform.localPosition = new Vector3(0, 1.5f, 0);
-            }
-            if (distance <= interactionRange)
-            {
-                if (Input.GetKeyDown(interactionKey))
+                PlayerInventory inventory = player.GetComponent<PlayerInventory>();
+                if (inventory != null && inventory.hasRustyKey)
                 {
-                    PlayerInventory inventory = player.GetComponent<PlayerInventory>();
-                    if (inventory != null && inventory.hasRustyKey)
-                    {
-                        TransformRustyKey(inventory);
-                    }
-                    else
-                    {
-                        Debug.Log("You need the Rusty Key to use this.");
-                    }
+                    TransformRustyKey(inventory);
                 }
-            }
-            else
-            {
-                if (interactionIconInstance != null)
+                else
                 {
-                    Destroy(interactionIconInstance);
+                    Debug.Log("You need the Rusty Key to use this.");
                 }
             }
         }
@@ -55,8 +36,10 @@ public class WashingKeys : MonoBehaviour
 
     private void TransformRustyKey(PlayerInventory inventory)
     {
+        // Remove Rusty Key
         inventory.hasRustyKey = false;
 
+        // Randomly choose a colored key
         float roll = Random.value;
 
         if (roll < 0.25f)
@@ -78,10 +61,6 @@ public class WashingKeys : MonoBehaviour
         {
             inventory.hasRedKey = true;
             Debug.Log("Your Rusty Key turned into a Red Key!");
-        }
-        if (interactionIconInstance != null)
-        {
-            Destroy(interactionIconInstance);
         }
     }
 }
