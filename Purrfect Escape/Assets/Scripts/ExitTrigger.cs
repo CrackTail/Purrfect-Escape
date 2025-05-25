65b9410a9fc112cda5b652e52bee18460643a129
Nagi408 <Nathan.Wathelet77@gmail.com>
2025-05-25 13:39:28 +0200

    Inventory updated and exits now work with the required colored key

--- Purrfect Escape/Assets/Scripts/ExitTrigger.cs
+++ Purrfect Escape/Assets/Scripts/ExitTrigger.cs
@@ -1,3 +1,51 @@
+//using UnityEngine;
+
+//public class ExitDoor : MonoBehaviour
+//{
+//    [SerializeField] private GameObject gameWinPanel;
+//    [SerializeField] private KeyCode interactionKey = KeyCode.E;
+//    [SerializeField] private float interactionRange = 4f;
+
+//    private GameObject player;
+
+//    private void Start()
+//    {
+//        player = GameObject.FindGameObjectWithTag("Player");
+//        if (gameWinPanel != null)
+//            gameWinPanel.SetActive(false);
+//    }
+
+//    private void Update()
+//    {
+//        if (player == null) return;
+
+//        float distance = Vector2.Distance(transform.position, player.transform.position);
+//        if (distance <= interactionRange)
+//        {
+//            if (Input.GetKeyDown(interactionKey))
+//            {
+//                // Check if player has the inventory component
+//                PlayerInventory inventory = player.GetComponent<PlayerInventory>();
+//                if (inventory != null && inventory.hasKey)
+//                {
+//                    TriggerWin();
+//                }
+//                else
+//                {
+//                    Debug.Log("You need the key to unlock the exit!");
+//                }
+//            }
+//        }
+//    }
+
+//    private void TriggerWin()
+//    {
+//        Time.timeScale = 0f;
+//        if (gameWinPanel != null)
+//            gameWinPanel.SetActive(true);
+//        Debug.Log("You Win!");
+//    }
+//}
 using UnityEngine;
 
 public class ExitDoor : MonoBehaviour
@@ -5,6 +53,8 @@ public class ExitDoor : MonoBehaviour
     [SerializeField] private GameObject gameWinPanel;
     [SerializeField] private KeyCode interactionKey = KeyCode.E;
     [SerializeField] private float interactionRange = 4f;
+
+    [SerializeField] private KeyType requiredKey = KeyType.GoldKey; // Choose which key unlocks the door!
 
     private GameObject player;
 
@@ -24,17 +74,31 @@ public class ExitDoor : MonoBehaviour
         {
             if (Input.GetKeyDown(interactionKey))
             {
-                // Check if player has the inventory component
                 PlayerInventory inventory = player.GetComponent<PlayerInventory>();
-                if (inventory != null && inventory.hasKey)
+                if (inventory != null && HasRequiredKey(inventory))
                 {
                     TriggerWin();
                 }
                 else
                 {
-                    Debug.Log("You need the key to unlock the exit!");
+                    Debug.Log($"You need the {requiredKey} to unlock the exit!");
                 }
             }
+        }
+    }
+
+    private bool HasRequiredKey(PlayerInventory inventory)
+    {
+        switch (requiredKey)
+        {
+            case KeyType.GoldKey: return inventory.hasKey;
+            case KeyType.PinkKey: return inventory.hasKeyPink;
+            case KeyType.RustyKey: return inventory.hasRustyKey;
+            case KeyType.BlueKey: return inventory.hasBlueKey;
+            case KeyType.OrangeKey: return inventory.hasOrangeKey;
+            case KeyType.PurpleKey: return inventory.hasPurpleKey;
+            case KeyType.RedKey: return inventory.hasRedKey;
+            default: return false;
         }
     }
 
@@ -45,4 +109,15 @@ public class ExitDoor : MonoBehaviour
             gameWinPanel.SetActive(true);
         Debug.Log("You Win!");
     }
+}
+
+public enum KeyType
+{
+    GoldKey,
+    PinkKey,
+    RustyKey,
+    BlueKey,
+    OrangeKey,
+    PurpleKey,
+    RedKey
 }
--- Purrfect Escape/Assets/Scripts/InventoryTracker.cs
+++ Purrfect Escape/Assets/Scripts/InventoryTracker.cs
@@ -8,6 +8,10 @@ public class PlayerInventory : MonoBehaviour
     private bool _hasKey = false;
     private bool _hasKeyPink = false;
     private bool _hasKeyRusty = false;
+    private bool _hasBlueKey = false;
+    private bool _hasOrangeKey = false;
+    private bool _hasPurpleKey = false;
+    private bool _hasRedKey = false;
 
     public bool hasFish
     {
@@ -72,6 +76,49 @@ public class PlayerInventory : MonoBehaviour
             if (_hasKeyRusty != value)
                 Debug.Log($"Inventory updated: Rusty Key = {value}");
             _hasKeyRusty = value;
+        }
+    }
+    public bool hasBlueKey
+    {
+        get => _hasBlueKey;
+        set
+        {
+            if (!_hasBlueKey && value)
+                Debug.Log("Collected Blue Key!");
+            _hasBlueKey = value;
+        }
+    }
+
+    public bool hasOrangeKey
+    {
+        get => _hasOrangeKey;
+        set
+        {
+            if (!_hasOrangeKey && value)
+                Debug.Log("Collected Orange Key!");
+            _hasOrangeKey = value;
+        }
+    }
+
+    public bool hasPurpleKey
+    {
+        get => _hasPurpleKey;
+        set
+        {
+            if (!_hasPurpleKey && value)
+                Debug.Log("Collected Purple Key!");
+            _hasPurpleKey = value;
+        }
+    }
+
+    public bool hasRedKey
+    {
+        get => _hasRedKey;
+        set
+        {
+            if (!_hasRedKey && value)
+                Debug.Log("Collected Red Key!");
+            _hasRedKey = value;
         }
     }
 }
